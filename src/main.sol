a70ce70a759db20416fac84f1d690bf085257c0d
Coderoftheworld <64337746+Coderoftheworld@users.noreply.github.com>
2021-04-17 12:19:23 -0400

    Merge branch 'main' of https://github.com/Masterprogrammers123/zylus into main

--- src/main.sol
+++ src/main.sol
@@ -1,24 +1,5 @@
 pragma solidity 0.8.3;
 
-
-contract Vote {
-    uint public phantomVotes = 0; 
-    uint public zackVotes = 0;
-
-    event incrementVote(uint value);
+contract Zylus {
     
-
-    function vote(uint numberOfvoter) payable public returns(string memory) {
-        if (numberOfvoter == 1) {
-            phantomVotes += 1;
-            emit incrementVote(phantomVotes);
-            return 'Voted for Phantom!';
-        } else if (numberOfvoter == 0) {
-            zackVotes += 1;
-            emit incrementVote(zackVotes);
-            return 'Voted for Zack!';
-        } else {
-            return "Didn't vote for anyone. Or u dint vote";
-        }
-    }
 }
